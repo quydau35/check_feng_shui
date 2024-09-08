8399ef5efd7b8b45e99aa005c56de7333d252da1
quydau35 <quydau35@gmail.com>
2024-09-08 23:46:38 +0700

    refactor

--- lib/features/scan_calculator/presenter/scanscreenscaffold.dart
+++ lib/features/scan_calculator/presenter/scanscreenscaffold.dart
@@ -9,20 +9,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_scalable_ocr/flutter_scalable_ocr.dart';
 
-class ScanScreenScaffold extends ConsumerStatefulWidget {
-  @override
-  ConsumerState<ScanScreenScaffold> createState() => _ScanScreenScaffoldState();
-}
-
-class _ScanScreenScaffoldState extends ConsumerState<ScanScreenScaffold> {
-  String text = "";
-
-  String snapshotData = '';
-
+class ScanScreenScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final AsyncValue<ContentData> content = ref.watch(contentProvider);
-    final rawContent = ref.watch(rawContentProvider);
     return Scaffold(
         appBar: AppBar(
           title: Text("Phong Thuy"),
@@ -31,41 +20,33 @@ class _ScanScreenScaffoldState extends ConsumerState<ScanScreenScaffold> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              ScalableOCR(
-                  paintboxCustom: Paint()
-                    ..style = PaintingStyle.stroke
-                    ..strokeWidth = 4.0
-                    ..color = const Color.fromARGB(153, 102, 160, 241),
-                  boxLeftOff: 5,
-                  boxBottomOff: 2.5,
-                  boxRightOff: 5,
-                  boxTopOff: 2.5,
-                  boxHeight: MediaQuery.of(context).size.height / 3,
-                  getRawData: (value) {
-                    inspect(value);
-                  },
-                  getScannedText: (value) {
-                    Future.delayed(Duration(milliseconds: 500));
-                    ref.read(rawContentProvider.notifier).state = value;
-                  }),
+              ScannerContainer(),
               ResultContainer(),
-              // StreamBuilder<String>(
-              //   stream: rawContent,
-              //   builder:
-              //       (BuildContext context, AsyncSnapshot<String> snapshot) {
-              //     if (snapshot.data != null) {
-              //       snapshotData = snapshot.data!;
-              //       ref
-              //           .read(contentRepositoryProvider)
-              //           .calculateRank(snapshotData);
-              //       ResultContainer(textInput: snapshot.data! ?? "");
-              //     }
-              //     return ResultContainer(
-              //         textInput: snapshot.data != null ? snapshot.data! : "");
-              //   },
-              // )
             ],
           ),
         ));
+  }
+}
+
+class ScannerContainer extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ScalableOCR(
+        paintboxCustom: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 4.0
+          ..color = const Color.fromARGB(153, 102, 160, 241),
+        boxLeftOff: 5,
+        boxBottomOff: 2.5,
+        boxRightOff: 5,
+        boxTopOff: 2.5,
+        boxHeight: MediaQuery.of(context).size.height / 3,
+        getRawData: (value) {
+          inspect(value);
+        },
+        getScannedText: (value) async {
+          await Future.delayed(Duration(milliseconds: 500));
+          ref.read(rawContentProvider.notifier).state = value;
+        });
   }
 }
